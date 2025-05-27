8299395ac0c5cd34096fd777638252f2ba18c442
Ali <alisajadi001010@gmail.com>
2025-05-28 07:23:51 +1400

    Stup project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <Button>Click</Button>
+    </>
+  );
+}
+
+export default page;
